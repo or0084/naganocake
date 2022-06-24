f99c2034cd0f9810f6fa5ae66fe87490b1fb739c
EC2 Default User <ec2-user@ip-172-31-5-37.ap-northeast-1.compute.internal>
2022-06-24 02:41:12 +0000

    [Add] 注文履歴詳細画面製作中

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -4,9 +4,21 @@ class Admin::OrderDetailsController < ApplicationController
   end
 
   def show
+    @order = OrderDetail.find(params[:id])
+    @orders = @order.item_id
   end
 
+  def update
+    @order = OrderDetail,find(params[:id])
+    @order.update(order_detail_params)
+    redirect_to admin_order_details_path
+  end
+
+
+  private
+
+
   def order_detail_params
-    params.require(:order_detail).permit(:making_status)
+    params.require(:order_detail).permit(:making_status, :order_status)
   end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,16 +1,24 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
+  belongs_to :item
 
-  enum status: {
-    入金待ち: 0,
-    入金確認: 1,
+  enum making_status: {
+    製作負荷: 0,
+    製作待ち: 1,
     製作中: 2,
-    発送準備中: 3,
-    発送済み: 4
+    製作完了: 3
   }
 
   def with_tax_price
    (price * 1.1).floor
+  end
+
+  def subtotal
+    order_detail.with_tax_price * amount
+  end
+
+  def total
+    order_detail.subtotal + order.shipping_cost
   end
 
 end
