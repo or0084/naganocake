0b0076609061d29bf97450a987342a11295c1870
EC2 Default User <ec2-user@ip-172-31-5-37.ap-northeast-1.compute.internal>
2022-06-25 11:53:06 +0000

    [Fix] 会員登録できないエラー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_admin!
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
-  end
 end
