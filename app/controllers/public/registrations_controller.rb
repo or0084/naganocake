bdb5a7f80f55373e41a2b71a09cce9f89cbd0eb2
EC2 Default User <ec2-user@ip-172-31-5-37.ap-northeast-1.compute.internal>
2022-07-08 07:33:58 +0000

    [Fix]　会員のログインできない

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -6,7 +6,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
 
   # before_action :configure_account_update_params, only: [:update]
 
-  def after_sign_out_path_for(resource)
+  def after_sign_up_path_for(resource)
     public_customer_path(resource)
   end
 
