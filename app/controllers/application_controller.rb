0a0f56afc19d181cbbd536acbcc0288001386299
EC2 Default User <ec2-user@ip-172-31-5-37.ap-northeast-1.compute.internal>
2022-07-12 08:45:49 +0000

    [Finish] 実装テスト完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope.is_a?(Customer)
-        customer_path(resource)
+        mypage_path
     else
         root_path
     end
